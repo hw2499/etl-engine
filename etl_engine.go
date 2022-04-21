01720d8286a98f75939d3c6c67acc8f36b427980
hw <hw2499@sohu.com>
2022-04-22 00:33:41 +0800

    modify package path

--- etl_engine.go
+++ etl_engine.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	di "etl-engine/dictionary"
 	"fmt"
+	di "github.com/hw2499/etl-engine/dictionary"
 )
 
 /*
 
  */
 func main() {
-	fmt.Println(di.GLOBAL_LOG_INFO_A)
+	fmt.Println(di.GLOBAL_LOG_INFO_B)
+	//fmt.Println(di.GLOBAL_LOG_INFO_A)
 	fmt.Println(di.GLOBAL_LOG_INFO)
 }
